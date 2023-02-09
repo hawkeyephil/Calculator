47011416869e4b387bb5edc40cbf4f4e62acdc25
hawkeyephil <pjcaldarella@quinnipiac.edu>
2023-02-09 17:44:09 -0500

    Multiplication and Division Added

--- Calculator.java
+++ Calculator.java
@@ -11,11 +11,16 @@ public class Calculator {
 		System.out.println("Enter an Integer");
 		int secondOperand = scannerObject.nextInt();
 
-        //Ask user to enter a 1 for addition or 2 for subtraction 
-		System.out.println("Enter 1 for addition and 2 for subtraction"); 
+        //Ask user to enter a 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division 
+		System.out.println("Enter one of the following numbers for the corresponding operation"); 
+        System.out.println("1: Addition");
+        System.out.println("2: Subtraction"); 
+        System.out.println("3: Multiplication");
+        System.out.println("4: Division");
         //Read in user input 
         int operation = scannerObject.nextInt(); 
-        //Return the result based on the operation selected
+        
+        //Return the result based on the operation selected and error message
         switch(operation) {
             //Addition 
             case 1: 
@@ -24,6 +29,18 @@ public class Calculator {
             //Subtraction
             case 2: 
                 System.out.println((firstOperand - secondOperand));
+                break; 
+            //Multiplication
+            case 3: 
+                System.out.println((firstOperand * secondOperand));
+                break; 
+            //Division
+            case 4: 
+                System.out.println((firstOperand/secondOperand));
+                break; 
+            //Error Message
+            default: 
+                System.out.println("Invalid entry"); 
                 break;
         }
     }
