8b4f539cd642cf446b8324129a17a1d5ee68435d
hawkeyephil <pjcaldarella@quinnipiac.edu>
2023-02-09 10:55:34 -0500

    Addition or subrtraction?

--- Calculator.java
+++ Calculator.java
@@ -2,11 +2,29 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        //Create a scanner object to read in user input 
         Scanner scannerObject = new Scanner(System.in);
+        //Ask for operand 1 and read in that integer 
 		System.out.println("Enter an Integer");
 		int firstOperand = scannerObject.nextInt();
+        //Ask for operand 2 and read in that integer 
 		System.out.println("Enter an Integer");
 		int secondOperand = scannerObject.nextInt();
-		System.out.println((firstOperand + secondOperand));
+
+        //Ask user to enter a 1 for addition or 2 for subtraction 
+		System.out.println("Enter 1 for addition and 2 for subtraction"); 
+        //Read in user input 
+        int operation = scannerObject.nextInt(); 
+        //Return the result based on the operation selected
+        switch(operation) {
+            //Addition 
+            case 1: 
+                System.out.println((firstOperand + secondOperand));
+                break;
+            //Subtraction
+            case 2: 
+                System.out.println((firstOperand - secondOperand));
+                break;
+        }
     }
 }
